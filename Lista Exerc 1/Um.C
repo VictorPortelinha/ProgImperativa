c55ba0a323288342b71ac06d3925e80355b9654f
Victor Portelinha <vcportelinha@gmail.com>
2023-08-04 20:20:30 -0300

    meu primeiro commit

---
+++ Lista Exerc 1/Um.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*This is a comment*/
+
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+    while(i<25){
+        printf("%d\t\n",i);
+        i++;
+    }
+
+    return 0;
+}
